a931bf14b5a9a4d5d19b241a85db636edf9fd400
Matthias Loker <matthias.loker@hitfoxgroup.com>
2015-04-08 17:46:32 +0200

    Remove permitted domains

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,7 +2,7 @@ class CallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
     user = User.from_omniauth(request.env["omniauth.auth"])
-    if user.has_permitted_domain? && user.save
+    if user.save
       flash[:notice] = "Successfully signed in."
       sign_in_and_redirect user
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,6 @@ class User < ActiveRecord::Base
   has_many :exercise_statuses
   has_many :exercises, through: :exercise_statuses
 
-  PERMITTED_DOMAINS = ['@hitfoxgroup.com', '@applift.com', '@finleap.com', '@appiris.com', '@dataconomy.com', '@ad2games.com', '@datamonk.com', '@finleap.com', '@finreach.com', '@savedo.com', '@billfront.com', '@pubnative.com', '@zeotap.com']
-
-  def has_permitted_domain?
-    PERMITTED_DOMAINS.include?(self.email.match(/@.*/).to_s)
-  end
-
   def self.from_omniauth(auth)
     user = User.find_or_initialize_by(email: auth.info.email)
     user.email = auth.info.email
