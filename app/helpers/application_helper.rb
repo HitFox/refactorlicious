91de907b739a13ec04b83f9ffa33ef3af55e9bf6
Mupat <dev@mupat.net>
2015-04-13 13:46:27 +0200

    remove leading whitespaces in pre blocks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,16 +9,18 @@ module ApplicationHelper
     end
   end
 
-  class CodeRayify < Redcarpet::Render::HTML
-    def block_code(code, language)
-      CodeRay.scan(code, language).div
-    end
-  end
-
   def markdown(text)
-    renderer ||= CodeRayify.new(filter_html: true, hard_wrap: true)
+    renderer ||= Redcarpet::Render::HTML.new(filter_html: true, hard_wrap: true)
     options = { strikethrough: true, fenced_code_blocks: true, no_intra_emphasis: true }
     md ||= Redcarpet::Markdown.new(renderer, options)
     md.render(text).html_safe
+  end
+
+  def highlight(code, language = :ruby)
+    CodeRay.scan(code, language).html(
+      :wrap => nil,
+      :line_numbers => :inline,
+      :css => :style
+    ).html_safe
   end
 end
