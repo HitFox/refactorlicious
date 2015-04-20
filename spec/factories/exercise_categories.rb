23762ed9ec7201b591bfe66c7c369a9c70cf7b2a
Joan <joan.martinez.ripoll@gmail.com>
2015-04-20 12:17:10 +0200

    fixed editor_spec.rb test

--- spec/factories/exercise_categories.rb
+++ spec/factories/exercise_categories.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :exercise_category do
-    name "Inline Method"
+    name "Introduce Explaining Variable"
     description "Sometimes you come across a method in which the body is as clear as the name. In this case it makes sense to get rid of the method\n\n1. Check that the method is not polymorphic.\n\n2. Find all calls to the method.\n\n3.Replace each call with the method body.\n"
     example "def get_rating\n  more_than_five_late_deliveries ? 2 : 1\nend\n\ndef more_than_five_late_deliveries\n  @number_of_late_deliveries > 5\nend\n\n# ||\n# \\/\n\ndef get_rating\n  @number_of_late_deliveries > 5 ? 2 : 1\nend\n"
   end
