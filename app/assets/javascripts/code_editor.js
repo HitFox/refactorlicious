31d38cb84b7dc64f2e3d8598269ecbde2968e246
Joan <joan.martinez.ripoll@gmail.com>
2015-04-22 12:26:43 +0200

    first version of submit

--- app/assets/javascripts/code_editor.js
+++ app/assets/javascripts/code_editor.js
@@ -8,7 +8,7 @@ $(document).on('page:change', function(){
   editor.setShowPrintMargin(false);
   document.getElementById('wrapper').style.fontSize='15px';
 
-  // Updating local Storage
+   // Updating local Storage
   if(localStorage)
   {
     if(localStorage.getItem(localStorageID)){
@@ -20,30 +20,38 @@ $(document).on('page:change', function(){
   }
 
   $(".submit a").click( function() {
-    var is_valid_answer = true;
-    var next_exercise_id = $(this).data("next-exercise");
-    var expected_words = $(this).data("expected-words");
-    var answer = localStorage.getItem(localStorageID);
 
-    if (answer) {
-      for (var word in expected_words) {
-        var reg_exp = new RegExp(word , 'g')
-        var count = (answer.match(reg_exp) || []).length;
-        if (expected_words[word] != count) {
-          is_valid_answer = false;
-        }
-      }
-    }
-    else {
-      is_valid_answer = false;
-    }
+    $(".success").show();
+   //  var is_valid_answer = true;
+   //  var next_exercise_id = $(this).data("next-exercise");
+   //  var expected_words = $(this).data("expected-words");
+   //  var answer = localStorage.getItem(localStorageID);
 
-    if(!is_valid_answer) {
-      alert ("Please review your code, your refactoring is still not complete!")
-    }
-    else {
-      alert ("Nice refactoring!")
-      $(location).attr('href',"/exercises/" + next_exercise_id);
-    }
+   //  if (answer) {
+   //    for (var word in expected_words) {
+   //      var reg_exp = new RegExp(word , 'g')
+   //      var count = (answer.match(reg_exp) || []).length;
+   //      if (expected_words[word] != count) {
+   //        is_valid_answer = false;
+   //      }
+   //    }
+   //  }
+   //  else {
+   //    is_valid_answer = false;
+   //  }
+
+   // $(".success").show();
+
+    // if(!is_valid_answer) {
+    //   alert ("Please review your code, your refactoring is still not complete!")
+    // }
+    // else {
+    //   alert ("Nice refactoring!")
+    //   $(location).attr('href',"/exercises/" + next_exercise_id);
+    // }
   });
+
+  $(".next").click( function() {
+    $(".success").hide();
+  })
 });
