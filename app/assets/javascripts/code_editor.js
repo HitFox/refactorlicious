113cbb56820d821f237f9af44ed4e3efc0ad6dfe
Matthias Loker <matthias.loker@hitfoxgroup.com>
2015-04-29 13:02:01 +0200

    Refactor editorjs
    * The editor now only loads on /exercises/*

--- app/assets/javascripts/code_editor.js
+++ app/assets/javascripts/code_editor.js
@@ -1,57 +1,72 @@
-$(document).on('page:change', function(){
+$(document).on('page:change', function() {
+  if(onExercisesShow()) {
+    initializeEditor();
+    updateLocalStorage();
+    installSubmitHandler();
+  }
+});
+
+function initializeEditor() {
   window.editor = ace.edit("wrapper");
-  var localStorageID = $('.editor').attr('data-storage-id')
   editor.setTheme("ace/theme/monokai");
   editor.getSession().setTabSize(2);
   editor.getSession().setMode("ace/mode/ruby");
   editor.getSession().setUseWrapMode(true);
   editor.setShowPrintMargin(false);
   document.getElementById('wrapper').style.fontSize='15px';
+}
 
-   // Updating local Storage
+function updateLocalStorage() {
+  var localStorageID = $('.editor').attr('data-storage-id');
   if(localStorage)
   {
-    if(localStorage.getItem(localStorageID)){
+    if(localStorage.getItem(localStorageID)) {
       editor.setValue(localStorage.getItem(localStorageID));
     }
     editor.getSession().on('change', function(e) {
       localStorage.setItem(localStorageID, editor.getValue());
     });
   }
+}
 
-  $(".submit").click( function() {
-
-    var expected_words = JSON.parse($("#key_words").text());
-    var answer = localStorage.getItem(localStorageID);
-    var is_valid_answer = true;
-
-    if (answer) {
-      for (var word in expected_words) {
-        var reg_exp = new RegExp(word , 'g')
-        var count = (answer.match(reg_exp) || []).length;
-        if (expected_words[word] !== count) {
-          is_valid_answer = false;
-        }
-      }
-    }
-    else {
-      is_valid_answer = false;
-    }
-
-    $("#message").show();
-
-    if(!is_valid_answer) {
-      $("#fail").show();
-    }
-    else {
-       $("#success").show();
-       $.ajax({
-        url: window.location.href + "/mark_as_finished",
-        method: "PATCH" });
-    }
-  });
+function installSubmitHandler() {
+  $(".submit").click(checkIfValidAnswer);
 
   $("#close").click( function() {
     $("#fail, #success, #message").hide();
   })
-});
+}
+
+function checkIfValidAnswer() {
+  var localStorageID = $('.editor').attr('data-storage-id');
+  var expected_words = JSON.parse($("#key_words").text());
+  var answer = localStorage.getItem(localStorageID);
+  var is_valid_answer = true;
+
+  if (answer) {
+    for (var word in expected_words) {
+      var reg_exp = new RegExp(word , 'g')
+      var count = (answer.match(reg_exp) || []).length;
+      if (expected_words[word] !== count) {
+        is_valid_answer = false;
+      }
+    }
+  } else {
+    is_valid_answer = false;
+  }
+
+  $("#message").show();
+
+  if(!is_valid_answer) {
+    $("#fail").show();
+  } else {
+     $("#success").show();
+     $.ajax({
+       url: window.location.href + "/mark_as_finished",
+       method: "PATCH" });
+  }
+}
+
+function onExercisesShow() {
+  return window.location.pathname.match(/^\/exercises\/\d+/) !== null;
+}
